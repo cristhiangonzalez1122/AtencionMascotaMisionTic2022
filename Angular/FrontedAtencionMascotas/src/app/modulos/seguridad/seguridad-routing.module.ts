f691a400e04d8449c5a5f4f1d2c4f66b6427b391
cristhian gonzalez <cristhianmg1122@gmail.com>
2021-11-30 01:07:10 -0500

    Se crea cuadro de logueo de identificacion de usuario con css y bootstrap

--- Angular/FrontedAtencionMascotas/src/app/modulos/seguridad/seguridad-routing.module.ts
+++ Angular/FrontedAtencionMascotas/src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
+import { IdentificacionComponent } from './identificacion/identificacion.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
 const routes: Routes = [
   {
     path:'cambio-clave',
     component:CambioClaveComponent
+  },
+  {
+    path:'identificacion',
+    component:IdentificacionComponent
+  },
+  {
+    path:'recuperar-clave',
+    component:RecuperarClaveComponent
   }
 ];
 
