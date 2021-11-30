597562596e5af8c724c48f34ad41c4800ce740d7
cristhian gonzalez <cristhianmg1122@gmail.com>
2021-11-29 22:10:28 -0500

    creada la carpeta front de walter

--- Angular/FrontedAtencionMascotas/src/app/app-routing.module.ts
+++ Angular/FrontedAtencionMascotas/src/app/app-routing.module.ts
@@ -5,33 +5,16 @@ import { InicioComponent } from './plantilla/inicio/inicio.component';
 
 const routes: Routes = [
   {
-    path: "inicio",
+    path:"inicio",
     component: InicioComponent
   },
   {
-    path: "",
+    path:"",
     pathMatch: 'full',
     redirectTo: '/inicio'
   },
-
   {
-    path: 'seguridad',
-    loadChildren: () => import("./modulos/seguridad/seguridad.module").then(x => x.SeguridadModule)
-  },
-  {
-    path: 'administracion',
-    loadChildren: () => import("./modulos/administracion/administracion.module").then(x => x.AdministracionModule)
-  },
-  {
-    path: 'programar-visita',
-    loadChildren: () => import("./modulos/programar-visita/programar-visita.module").then(x => x.ProgramarVisitaModule)
-  },
-  {
-    path: 'visita-medico',
-    loadChildren: () => import("./modulos/visita-medico/visita-medico.module").then(x => x.VisitaMedicoModule)
-  },
-  {
-    path: '**',
+    path:'**',
     component: ErrorComponent
   }
 ];
--- Angular/FrontedAtencionMascotas/src/app/modulos/administracion/administracion-routing.module.ts
+++ Angular/FrontedAtencionMascotas/src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CrearPropietarioComponent } from './propietario/crear-propietario/crear-propietario.component';
-import { EditarPropietarioComponent } from './propietario/editar-propietario/editar-propietario.component';
 
-const routes: Routes = [
-  {
-    path: 'crear-propietario',
-    component: CrearPropietarioComponent
-  },
-  {
-    path: 'editar-propietario',
-    component: EditarPropietarioComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
