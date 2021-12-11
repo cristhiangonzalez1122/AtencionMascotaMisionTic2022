c21465162dfe7194cfbd09c2ac1590a38f388124
cristhian gonzalez <cristhianmg1122@gmail.com>
2021-12-11 01:32:31 -0500

    Se agregan modulos servicios propietario para empezar a hacer el crud

---
+++ Angular/FrontedAtencionMascotas/src/app/modelos/propietario.modelo.ts
@@ -0,0 +1,11 @@
+export  class ModeloPropietario{
+    id?: string;
+    identificacion?: string;
+    nombres?: string;
+    apellidos?: string;
+    telefono?: string;
+    direccion?: string;
+    correo?: string;
+
+
+}
